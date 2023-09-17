70f285d55aef2ec653efde4efbbf6ae365291941
hobagi01 <139134134+hobagi01@users.noreply.github.com>
2023-09-18 03:14:46 +0900

    W2-assignment

--- src/w2/sketches/sketch2/script/alertPromptConfirm.js
+++ src/w2/sketches/sketch2/script/alertPromptConfirm.js
@@ -1,3 +1,9 @@
-alert('나의 홈페이지에 당도한 것을 환영하오. 낯선 이여.');
-let userTyped = prompt('자네, 이름이 무엇인가?', '홍길동');
-confirm('그러니깐, 너의 이름이 ' + userTyped + '이라는 거지?');
+alert('안녕하세요? 혜영이의 홈페이지에 오신 것을 환영합니다.');
+prompt('샌즈 아시나요?');
+let userInputFromPrompt;
+userInputFromPrompt = prompt('당신의 이름은?', '홍길동');
+confirm('와 아시는구나 샌즈!');
+let confirmVal = confirm('ㅇㅇ 너의 이름은 ' + userInputFromPrompt + '이구나.');
+if (confirmVal == true) {
+  alert('환영합니다 ' + userInputFromPrompt + '님.');
+}
